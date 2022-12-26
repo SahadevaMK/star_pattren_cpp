27c57d78138bebf66c7506c93bcf89ba1ea0fa5a
SahadevaMK <sahadevamk121@gmail.com>
2022-12-27 01:14:56 +0530

    hollowrectopposite

---
+++ hollowrectpractice.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,row,col;
+    cout <<"enter the row and column" <<endl;
+    cin >> row >> col;
+    for (i=1;i<=5;i++){
+        for(j=1;j<=4;j++){
+            if(j==1 || j==4){
+                cout << "*";
+            }
+            else if(i==2 || i==3 || i==4){
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
